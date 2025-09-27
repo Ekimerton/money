30b272a5b196b0f2d8659a380817cebd9c2eedb7
Ekim Karabey <ekim0252@gmail.com>
2025-09-27 15:41:25 -0700

    Add back theme transition

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default async function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <SidebarProvider>
             <ShellLayout>
               {children}
